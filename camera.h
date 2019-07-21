1e665712b571d9bf8c57aa7a878d52089052aeb8
carlos-lopez-garces <carlos@hover.to>
2019-07-21 16:25:49 -0700

    Add positionable camera

--- camera.h
+++ camera.h
@@ -5,11 +5,26 @@
 
 class camera {
 public:
-  camera() {
-    lower_left_corner = vec3(-2.0, -1.0, -1.0);
-    horizontal = vec3(4.0, 0.0, 0.0);
-    vertical = vec3(0.0, 2.0, 0.0);
-    origin = vec3(0.0, 0.0, 0.0);
+  // vup is the world's view up vector. vfov is in degrees.
+  camera(vec3 lookfrom, vec3 lookat, vec3 vup, float vfov, float aspect) {
+    vec3 u, v, w;
+
+    // Convert to radians.
+    float theta = vfov*M_PI/180;
+    float half_height = tan(theta/2);
+    float half_width = aspect * half_height;
+
+    origin = lookfrom;
+    // w points from the scene towards the camera.
+    w = unit_vector(lookfrom-lookat);
+    // uv is the image's plane. w is normal to that plane; it is tilted towards the lookat point.
+    u = unit_vector(cross(vup, w));
+    v = cross(w, u);
+
+    lower_left_corner = vec3(-half_width, -half_height, -1.0);
+    lower_left_corner = origin - half_width*u - half_height*v - w;
+    horizontal = 2*half_width*u;
+    vertical = 2*half_height*v;
   }
 
   ray get_ray(float u, float v) {
--- list_ray_intersection.cpp
+++ list_ray_intersection.cpp
@@ -44,7 +44,7 @@ int main() {
   list[4] = new sphere(vec3(-1,0,-1), -0.45, new dielectric(1.5));
   hitable *world = new hitable_list(list, 5);
 
-  camera cam;
+  camera cam(vec3(-1,1,1), vec3(0,0,-1), vec3(0,1,0), 45, float(nx)/float(ny));
 
   for (int j = ny-1; j >= 0; j--) {
     for (int i = 0; i < nx; i++) {
