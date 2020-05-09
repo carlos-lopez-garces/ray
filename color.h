4328741ac617062c07f4c203a4e7faf3a3ce985d
Carlos López Garcés <carlos.lopez.garces@gmail.com>
2020-05-09 00:14:04 -0700

    Add color.h

---
+++ color.h
@@ -0,0 +1,14 @@
+#ifndef COLOR_H
+#define COLOR_H
+
+#include "vec3.h"
+
+#include <iostream>
+
+void write_color(std::ostream &out, color pixel_color) {
+  out << static_cast<int>(255.999 * pixel_color.r()) << ' '
+    << static_cast<int>(255.999 * pixel_color.g()) << ' '
+    << static_cast<int>(255.999 * pixel_color.b()) << '\n';
+}
+
+#endif
