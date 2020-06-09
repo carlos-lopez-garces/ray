758ff04a17333b2e49011566f67a303771bcf72f
Carlos López Garcés <carlos.lopez.garces@gmail.com>
2020-06-08 22:36:38 -0700

    Add random number generator

--- common.h
+++ common.h
@@ -5,7 +5,7 @@
 #include <cstdlib>
 #include <limits>
 #include <memory>
-
+#include <random>
 
 // Usings.
 
@@ -22,6 +22,16 @@ const double pi = 3.1415926535897932385;
 
 inline double degrees_to_radians(double degrees) {
   return degrees * pi / 180;
+}
+
+inline double random_double() {
+  static std::uniform_real_distribution<double> distribution(0.0, 1.0);
+  static std::mt19937 generator;
+  return distribution(generator);
+}
+
+inline double random_double(double min, double max) {
+  return min + (max - min) * random_double();
 }
 
 // Common headers.
