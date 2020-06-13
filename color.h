1fbdac9fd17e508698ef774ce3ed7b3a96c90e9a
Carlos López Garcés <carlos.lopez.garces@gmail.com>
2020-06-13 16:47:59 -0700

    Add gamma correction

--- color.h
+++ color.h
@@ -11,9 +11,12 @@ void write_color(std::ostream &out, color pixel_color, int samples_per_pixel) {
   auto b = pixel_color.z();
 
   auto scale = 1.0 / samples_per_pixel;
-  r *= scale;
-  g *= scale;
-  b *= scale;
+  auto gamma = 2.0;
+
+  // Gamma-corrected color.
+  r = pow(r * scale, 1/gamma);
+  g = pow(g * scale, 1/gamma);
+  b = pow(b * scale, 1/gamma);
 
   out << static_cast<int>(256 * clamp(r, 0.0, 0.999)) << ' '
     << static_cast<int>(256 * clamp(g, 0.0, 0.999)) << ' '
