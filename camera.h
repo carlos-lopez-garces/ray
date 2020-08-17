106fb084ede39c62c761ddfb4bf3fb6035d80e8c
Carlos López Garcés <carlos.lopez.garces@gmail.com>
2020-08-17 16:17:21 -0700

    Add position and orientation to camera

--- camera.h
+++ camera.h
@@ -17,41 +17,26 @@ vec3 random_in_unit_disk() {
 class camera {
 public:
 
-  camera() {
-    auto aspect_ratio = 16.0 / 9.0;
-    auto viewport_height = 2.0;
+  // vfov is the vertical field of view.
+  camera(point3 lookfrom, point3 lookat, vec3 vup, double vfov, double aspect_ratio) {
+    auto theta = degrees_to_radians(vfov);
+    // tan is not a linear map, so you can't do just tan(theta).
+    auto viewport_height = 2.0 * tan(theta / 2);
     auto viewport_width = aspect_ratio * viewport_height;
-    auto focal_length = 1.0;
 
-    origin = point3(0, 0, 0);
-    horizontal = vec3(viewport_width, 0.0, 0.0);
-    vertical = vec3(0.0, viewport_height, 0.0);
+    auto w = unit_vector(lookfrom - lookat);
+    // uv is the image's plane. w is normal to that plane; it is tilted towards the lookat point.
+    auto u = unit_vector(cross(vup, w));
+    auto v = cross(w, u);
+
+    origin = lookfrom;
+    horizontal = viewport_width * u;
+    vertical = viewport_height * v;
 
     // Lower left corner of the "film" or image plane.
     lower_left_corner 
-      = origin - horizontal / 2 - vertical / 2 - vec3(0.0, 0.0, focal_length);
+      = origin - horizontal / 2 - vertical / 2 - w;
   }
-
-  //// vup is the world's view up vector. vfov is in degrees.
-  //camera(vec3 lookfrom, vec3 lookat, vec3 vup, float vfov, float aspect, float aperture, float focus_dist) {
-  //  lens_radius = aperture/2;
-
-  //  // Convert to radians.
-  //  float theta = vfov*pi/180;
-  //  float half_height = tan(theta/2);
-  //  float half_width = aspect * half_height;
-
-  //  origin = lookfrom;
-  //  // w points from the scene towards the camera.
-  //  w = unit_vector(lookfrom-lookat);
-  //  // uv is the image's plane. w is normal to that plane; it is tilted towards the lookat point.
-  //  u = unit_vector(cross(vup, w));
-  //  v = cross(w, u);
-
-  //  lower_left_corner = origin - half_width*focus_dist*u - half_height*focus_dist*v - focus_dist*w;
-  //  horizontal = 2*half_width*focus_dist*u;
-  //  vertical = 2*half_height*focus_dist*v;
-  //}
 
   ray get_ray(double u, double v) const {
     // Note the 2 terms of the 2nd argument: the vector subtraction yields a 
